9c8d3113c0e961b07287c84588a590fbee6a73fb
Arthursenai <123407087+Arthursenai@users.noreply.github.com>
2023-03-29 19:34:53 -0300

    tentativa 1 de iniciar só depois de pressionar uma

--- assets/extra.js
+++ assets/extra.js
@@ -1,44 +1,59 @@
 
 const boneco = document.querySelector('.boneco');
 const squ = document.querySelector('.squ');
+textstart = document.querySelector('text-start');
 
+
+
+
+/*================ Função Start ===================*/ 
+
+const start= () => {
+
+    document.getElementById("text-start").style.color = "white";
+
+    squ.classList.add('squ-animation');
+
+    boneco.src = '/assets/images/mantis animation.gif';
+    boneco.style.width = '400px';
+    document.addEventListener('keydown', start);
+}
+
+document.addEventListener('keydown', start);
+/*
 const an= () => {
     boneco.classList.add('an');
     setTimeout(()=>{
         boneco.classList.remove('an');
+      
 
     },500)
 }
+/*document.addEventListener('keydown', an);*/
 /*
-const loop = setInterval(() => {
-
-    const squPosition = squ.offsetleft;
-    const bonecoPosition = +window.getComputedStyle(boneco).bottom.replace('px', '');
-
-    if (squPosition <= 100 && squPosition > 0 && bonecoPosition < 80){
-
-        squ.style.animation = 'none';
-        squ.style.left = `${squPosition}px`;
-
-        boneco.style.animation = 'none';
-        boneco.style.bottom = `${bonecoPosition}px`;
-
-    }
-    
-}, 10);*/
-
 var colisao = setInterval( function(){
     var bonecoPosition = parseInt(
         window.getComputedStyle(boneco).getPropertyValue('top')
     )
     var squPosition = parseInt(
-        window.getComputedStyle(squ).getPropertyValue('right')
+        window.getComputedStyle(squ).getPropertyValue('left')
     )
 
-    if(squPosition <= 80 && squPosition > 0 && bonecoPosition >=0){
+    if(squPosition <= 7 && squPosition > 0 && bonecoPosition >=180){
         squ.style.animation = 'none'
         squ.style.display = 'none'
-        alert('Você Morreu')
+        boneco.style.display = 'none'
+        document.getElementById("text-start").innerHTML="<strong>GAME OVER</strong>";  
     }
 }, 10)
-document.addEventListener('keydown', an);
+
+
+
+
+
+
+
+
+
+
+
